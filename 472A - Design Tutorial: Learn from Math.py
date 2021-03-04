3804366233c7620cd3fd08087fe5c7af2c381a91
shan-mathi <67865135+shan-mathi@users.noreply.github.com>
2021-03-04 19:57:43 +0530

    Create 472A - Design Tutorial: Learn from Math.py

---
+++ 472A - Design Tutorial: Learn from Math.py
@@ -0,0 +1,7 @@
+#109087175	Mar/04/2021 19:56UTC+5.5	Shan_XD	472A - Design Tutorial: Learn from Math	PyPy 3	Accepted	93 ms	0 KB
+
+n = int(input())
+if n%2==0:
+    print("{} {}".format(4,n-4))
+else:
+    print("{} {}".format(9, n - 9))
